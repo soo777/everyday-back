e9152ae82e3f6d5a810889ebef53643a9082679d
soo777 <ssm19944@gmail.com>
2020-08-20 18:30:21 +0900

    everyday-back 프로젝트 생성

---
+++ src/main/java/com/everyday/hello/hello.java
@@ -0,0 +1,12 @@
+package com.everyday.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+    @GetMapping("/test")
+    public String hello() {
+        return "Hello Super Coding World!!";
+    }
+}
