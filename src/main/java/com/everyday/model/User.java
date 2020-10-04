c07f824da4396b527881862030ddf2103fcbcdc4
soo777 <ssm19944@gmail.com>
2020-10-04 21:06:56 +0900

    User table 생성

---
+++ src/main/java/com/everyday/model/User.java
@@ -0,0 +1,28 @@
+package com.everyday.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@ToString
+@Getter
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_key", insertable = false, updatable = false)
+    private int userKey;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    private String password;
+
+    @Column(name = "create_date")
+    private String createDate;
+}
